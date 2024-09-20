a9b33117c2d758a5012591c4cbcd91d59157bb60
Steven Lomon Lennartsson <steven.lennartsson@gmail.com>
2024-09-20 20:09:18 +0200

    Add files via upload

---
+++ SQL-essential-training-by-walter-shields/DML.sql
@@ -0,0 +1,9 @@
+/*
+Created by: Steven Lomon Lennartsson
+Create date: 9/20/2024
+
+DML | Inserting, Updating and Deleting data in the database
+*/
+
+-- Adding an artist to the DATABASE
+INSERT INTO Artist(Name) VALUES ('Jean Dawson')
