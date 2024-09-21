f2ae25af23d042f5ca98abaacd959716f1bcdd49
Steven Lomon Lennartsson <steven.lennartsson@gmail.com>
2024-09-21 07:41:48 +0200

    Update DML.sql

--- SQL-essential-training-by-walter-shields/DML.sql
+++ SQL-essential-training-by-walter-shields/DML.sql
@@ -5,5 +5,13 @@ Create date: 9/20/2024
 DML | Inserting, Updating and Deleting data in the database
 */
 
--- Adding an artist to the DATABASE
+-- Adding an artist to the database
 INSERT INTO Artist(Name) VALUES ('Jean Dawson')
+
+-- Updating an artist in the database. The WHERE clause is VERY IMPORTANT HERE as to not update 
+-- every single row in the table!
+UPDATE Artist SET NAME = 'Bob Marley' WHERE ArtistId = 276
+
+-- Deleting an artist in the database. Once again; WHERE is important as otherwise, EVERY SINGLE 
+-- RECORD will be deleted
+DELETE FROM Artist WHERE ArtistId = 276
