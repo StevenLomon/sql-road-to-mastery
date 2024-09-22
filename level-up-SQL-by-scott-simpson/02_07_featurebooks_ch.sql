5e16c4f3dcf71991668d7cc9f924595f6142c73a
Steven Lomon Lennartsson <steven.lennartsson@gmail.com>
2024-09-22 07:46:12 +0200

    Completed!

---
+++ level-up-SQL-by-scott-simpson/02_07_featurebooks_ch.sql
@@ -0,0 +1,106 @@
+-- Create a list of books to feature in an exhibit.
+
+-- Make a pick list of books published from 1890-1899 
+-- which are not currently checked out.
+
+-- This is so easy? 4 star difficulty?
+
+SELECT
+	*
+FROM
+	Books AS b
+INNER JOIN
+	Loans AS l
+ON
+	b.BookID = l.BookID
+WHERE
+	1890 < b.Published < 1899 AND
+	l.ReturnedDate IS 
+	
+-- Mid-query writing I see where the difficulty lies haha! We need to find books that are NOT in the 
+-- Loans table! This calls for an INNER LEFT JOIN, doesn't it? Books will still be our left table:
+
+SELECT
+	*
+FROM
+	Books AS b
+LEFT JOIN
+	Loans AS l
+ON
+	b.BookID = l.BookID
+WHERE
+	b.Published >= 1890 AND b.Published <= 1899
+ORDER BY 
+	Published
+
+-- I've gotten *something* but I can't confidently say this is what is asked for in the challenge tbh. Let's
+-- just make it super clear which books we're looking for; it's the books that have never been checked out as
+-- well as the books that have been checked out and also returned. These are the returned books:
+
+SELECT
+	b.Published,
+	b.Title,
+	b.Author,
+	COUNT(DISTINCT b.BookID) AS "Book count"
+FROM
+	Books AS b
+INNER JOIN
+	Loans AS l
+ON
+	b.BookID = l.BookID
+WHERE
+	b.Published >= 1890 AND b.Published <= 1899 AND
+	l.ReturnedDate IS NOT NULL
+GROUP BY
+	b.Title
+ORDER BY 
+	b.Published
+	
+-- Double check to see if book counts are correct. They are!
+/*SELECT
+	*
+FROM
+	Books
+WHERE 
+	Published >= 1890 AND Published <= 1899
+ORDER BY
+	Title*/
+	
+-- Now, back to the LEFT JOIN and combining it with the query we just wrote.
+-- We're using a LEFT JOIN to get all books, even if they have no corresponding entry in the Loans
+-- table (i.e., they've never been checked out)
+SELECT
+	b.Title,
+	b.Author,
+	b.Published,
+	COUNT(DISTINCT b.title)
+FROM
+	Books AS b
+LEFT JOIN
+	Loans AS l
+ON
+	b.BookID = l.BookID
+WHERE
+	b.Published >= 1890 AND b.Published <= 1899 
+	AND (l.BookID IS NULL OR l.ReturnedDate IS NOT NULL)
+GROUP BY
+	b.Title, b.Published, b.Author
+ORDER BY
+	b.Published;
+	
+-- I gave up on this one and looked at the solution in the video :)
+SELECT 
+	Title,
+	Barcode
+FROM 
+	Books
+WHERE
+	Published BETWEEN 1890 AND 1899 -- Inclusive range
+	AND (BookID NOT IN (
+	SELECT BookID 
+	FROM Loans 
+	WHERE ReturnedDate IS NULL))
+ORDER BY Title;
+
+-- Easy and simple. I'm impressed and eager to use this as inspiration to do future problems!
+
---
+++ level-up-SQL-by-scott-simpson/02_08_bookstats_ch.sql
@@ -0,0 +1,85 @@
+-- Create two reports about book statistics.
+
+-- Report 1: Show how many titles were published 
+-- in each year.
+
+-- Report 2: Show the five books that have been
+-- checked out the most.
+
+-- I'm gonna just straight to Report 2 because it's almost the opposite of the patrons that have checked out the least.
+-- I'm gonna try and do it without looking at that query:
+
+SELECT
+	b.Title,
+	b.Author,
+	b.Barcode,
+	COUNT(l.BookID) AS "Checkout count"
+FROM
+	Books AS b
+INNER JOIN
+	Loans AS l
+ON
+	b.BookID = l.BookID
+GROUP BY
+	b.Title
+ORDER BY "Checkout count" DESC
+LIMIT 5;
+
+-- First try! Now on to Report 1! Also doesn't sound too hard. Also uses COUNT and GROUP BY and I don't even think
+-- we need to do any joins:
+
+SELECT
+	Published AS "Publish year",
+	COUNT(Published) AS "Count"
+FROM
+	Books
+GROUP BY
+	Published
+	
+-- That was so incredibly easy? Did I miss something? The penultimate challenge was harder than the ultimate one
+
+-- Just watched the beginning of the video. I think the challenge in Report 1 might lie in the fact that I have
+-- the same book being counted more than once..
+-- In the video he says "counting only one copy of each title we have multiples of". Why is that not clearly written
+-- in the challenge brief in the text haha? We need to use DISTINCT
+
+SELECT
+	DISTINCT Title,
+	Published AS "Year"
+FROM
+	Books
+ORDER BY
+	Year
+
+-- This one has got me stuck! I take back what I wrote earlier!
+-- But okay, I think I've got something. Let's use the query above as our subquery
+
+SELECT
+	Published,
+	COUNT(DISTINCT Title) AS "Book count"
+FROM
+	Books
+GROUP BY
+	Published
+	
+-- No subquery was needed, only the pattern COUNT(DISTINCT Field) which I hadn't used so far neither in
+-- Walter Shield's course nor this one haha. I asked ChatGPT to combine these two queries:
+SELECT
+    Published,
+    COUNT(DISTINCT Title) AS "Book count",
+    GROUP_CONCAT(Title, ', ') AS "Book Titles"
+FROM
+    (SELECT DISTINCT Title, Published FROM Books) AS DistinctBooks -- Using a subquery in the FROM clause is called a derived table!
+GROUP BY
+    Published
+ORDER BY
+    "Book count" DESC;
+	
+-- These are two completely new concepts; derived tables and GROUP_CONCAT. They will need some time to marinade haha
+
+-- The solution from the video without GROUP_CONCAT (altho mentioned and removed again since not all SQL dialects
+-- have that keyword
+SELECT Published, COUNT(DISTINCT(Title)) AS PubCount
+FROM Books
+GROUP BY Published
+ORDER BY PubCount DESC;
